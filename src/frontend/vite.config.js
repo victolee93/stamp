5a89e1d5ce98e3a29d507f50d2cab439aa85ff25
victolee93 <victolee93@gmail.com>
2021-05-01 17:13:17 +0900

    Vuetify build in gradle script

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build : {
+    outDir: path.resolve(__dirname, "../main/resources/static")
+  },
   plugins: [vue()],
   define: { 'process.env': {} },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
