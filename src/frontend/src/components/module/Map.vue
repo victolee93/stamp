053f0ed56d6f9b7694b460d82c8e86da93d407cc
이우승 <victolee93@gmail.com>
2021-05-09 10:26:38 +0900

    main page layout - kakao map

--- src/frontend/src/components/module/Map.vue
+++ src/frontend/src/components/module/Map.vue
@@ -1,15 +1,40 @@
 <template>
-  <section class="section is-medium">
-    카카오 지도
-  </section>
+  <div id="map"></div>
 </template>
 
 <script>
 export default {
-  name: "Map"
+  name: "Map",
+  mounted() {
+    window.kakao && window.kakao.maps ?
+        this.initMap() :
+        this.addScript();
+  },
+  methods : {
+    initMap() {
+      var container = document.getElementById('map');
+      var options = {
+        center: new kakao.maps.LatLng(37.478319, 126.951577),
+        level: 6
+      };
+      new kakao.maps.Map(container, options);
+    },
+
+    addScript() {
+      const jsKey = '7175a2b5e64296cba044869604551482';
+      const script = document.createElement('script');
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=' + jsKey;
+      document.head.appendChild(script);
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+#map {
+  width: 100%;
+  height: 700px;
+}
 </style>
