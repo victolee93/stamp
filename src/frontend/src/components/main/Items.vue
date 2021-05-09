797c0a7ef67f79e0b939e1f2970ed437a8b66edd
이우승 <victolee93@gmail.com>
2021-05-09 10:37:22 +0900

    main page layout - Items

--- src/frontend/src/components/main/Items.vue
+++ src/frontend/src/components/main/Items.vue
@@ -5,6 +5,18 @@
       <a><img src="../../assets/img/디라이트_대지_1.png"></a>
       <a><img src="../../assets/img/뜨갯빛나날_대지_1.png"></a>
       <a><img src="../../assets/img/리메이크_대지_1.png"></a>
+      <a><img src="../../assets/img/모니키키_대지_1.png"></a>
+      <a><img src="../../assets/img/모드니_대지_1.png"></a>
+      <a><img src="../../assets/img/베오트_대지_1.png"></a>
+      <a><img src="../../assets/img/센트마리아쥬_대지_1.png"></a>
+      <a><img src="../../assets/img/아란스토리_대지_1.png"></a>
+      <a><img src="../../assets/img/아토우플로어_대지_1.png"></a>
+      <a><img src="../../assets/img/오드뚜아_대지_1.png"></a>
+      <a><img src="../../assets/img/윤아트_대지_1.png"></a>
+      <a><img src="../../assets/img/클레이그라운드_대지_1.png"></a>
+      <a><img src="../../assets/img/호태노_대지_1.png"></a>
+      <a><img src="../../assets/img/후디무디_대지_1.png"></a>
+
     </p>
 
     <div class="panel-block">
@@ -33,6 +45,85 @@
         <img src="../../assets/img/리메이크_대지_1.png">
       </span>
       <span>리메이크 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/모니키키_대지_1.png">
+      </span>
+      <span>모니키키 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/모드니_대지_1.png">
+      </span>
+      <span>모드니 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/베오트_대지_1.png">
+      </span>
+      <span>베오트 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/센트마리아쥬_대지_1.png">
+      </span>
+      <span>센트마리아쥬 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/아란스토리_대지_1.png">
+      </span>
+      <span>아란스토리 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/아토우플로어_대지_1.png">
+      </span>
+      <span>아토우플로어 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/오드뚜아_대지_1.png">
+      </span>
+      <span>오드뚜아 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/윤아트_대지_1.png">
+      </span>
+      <span>윤아트 info</span>
+    </div>
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/클레이그라운드_대지_1.png">
+      </span>
+      <span>클레이그라운드 info</span>
+    </div>
+
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/호태노_대지_1.png">
+      </span>
+      <span>호태노 info</span>
+    </div>
+
+
+    <div class="panel-block">
+      <span class="image is-32x32" href="#">
+        <img src="../../assets/img/후디무디_대지_1.png">
+      </span>
+      <span>후디무디 info</span>
     </div>
   </nav>
 </template>
